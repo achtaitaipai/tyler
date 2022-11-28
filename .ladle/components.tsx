3a25709d1517a3a61454a92ac1fe42879e43a5e3
achtaitaipai <charles.cailleteau@gmail.com>
2022-11-28 18:06:07 +0100

    chore: configure ladle

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -3,8 +3,5 @@ import React from 'react'
 import '../src/style/index.css'
 
 export const Provider: GlobalProvider = ({ children, globalState }) => (
-	<>
-		<h1>Theme: {globalState.theme}</h1>
-		{children}
-	</>
+	<>{children}</>
 )
