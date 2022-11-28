dfdf6870dffabc96b328db31fe4e4c9436680804
achtaitaipai <charles.cailleteau@gmail.com>
2022-11-28 14:58:22 +0100

    chore: init
    init vite project and add dependencies
    
    BREAKING CHANGE: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:@typescript-eslint/strict',
+		'prettier',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'react/react-in-jsx-scope': 0,
+	},
+}
---
+++ .ladle/components.tsx
@@ -0,0 +1,10 @@
+import type { GlobalProvider } from '@ladle/react'
+import React from 'react'
+import '../src/style/index.css'
+
+export const Provider: GlobalProvider = ({ children, globalState }) => (
+	<>
+		<h1>Theme: {globalState.theme}</h1>
+		{children}
+	</>
+)
