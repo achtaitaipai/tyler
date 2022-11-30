4453052f8131b3b890f779eede40b171a22c5a06
achtaitaipai <charles.cailleteau@gmail.com>
2022-11-30 17:01:51 +0100

    feat(font): lato font

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import ThemeProvider from './lib/ui/ThemeProvider'
 import './style/index.css'
+import '@fontsource/lato/300.css'
+import '@fontsource/lato/400.css'
+import '@fontsource/lato/700.css'
+import '@fontsource/lato/900.css'
 
 const root = document.querySelector<HTMLElement>('#root')
 
