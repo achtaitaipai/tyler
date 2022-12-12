3f1ae4492da9ce6693c53f53e7c7a80591772c5c
achtaitaipai <charles.cailleteau@gmail.com>
2022-12-12 17:27:06 +0100

    top level await

--- src/lib/ui/ToggleTool/toggle.stories.tsx
+++ src/lib/ui/ToggleTool/toggle.stories.tsx
@@ -1,10 +1,14 @@
 import { BorderLeftIcon } from '@radix-ui/react-icons'
+import { useState } from 'react'
 import Toggle from '.'
 
-export const story = () => (
-	<Toggle>
-		{' '}
-		<BorderLeftIcon />
-	</Toggle>
-)
+export const story = () => {
+	const [enabled, setEnabled] = useState(false)
+	return (
+		<Toggle enabled={enabled} setEnabled={setEnabled}>
+			{' '}
+			<BorderLeftIcon />
+		</Toggle>
+	)
+}
 story.storyName = 'toggle tool'
